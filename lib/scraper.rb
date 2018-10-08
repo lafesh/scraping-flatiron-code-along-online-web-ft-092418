8ad5af6f53ec8d27b21b1d8a5bf6e6cab77ab3ba
Gina Fedder <ginafedder@yahoo.com>
2018-10-08 22:27:50 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require_relative './course.rb'
 
 class Scraper
   
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -15,7 +15,20 @@ class Scraper
       end
     end
   end
-  
+ 
+ def get_page
+   
+ end 
+ 
+ def get_courses
+   
+ end 
+ 
+ def make_courses(title, schedule, description)
+   
+ end 
+ 
+ 
 end
 
 
