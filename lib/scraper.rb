093dbfeccb766f3aafddde849165c6903383b3ed
Gina Fedder <ginafedder@yahoo.com>
2018-10-08 22:51:15 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,7 +13,7 @@ class Scraper
    self.get_page.css(".post")
  end 
  
- def make_courses(title, schedule, description)
+ def make_courses
    self.get_courses.each do |post|
     course = Course.new
     course.title = post.css("h2").text
