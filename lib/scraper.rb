4afb973a9cdbfb1a91177d2a0ac92a39747bab15
Gina Fedder <ginafedder@yahoo.com>
2018-10-08 22:30:56 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,21 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
- 
  def get_page
    
  end 
@@ -28,7 +17,16 @@ class Scraper
    
  end 
  
- 
+ def print_courses
+  self.make_courses
+  Course.all.each do |course|
+    if course.title
+      puts "Title: #{course.title}"
+      puts "  Schedule: #{course.schedule}"
+      puts "  Description: #{course.description}"
+    end
+  end
+  end
 end
 
 
